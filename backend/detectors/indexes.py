74f63959109b377b794c1957fdbfd6d604414ca3
LuisArias <luisariasllaguno@gmail.com>
2026-05-08 13:19:32 -0600

    Detectar llaves foráneas sin Índice
    - Se creó el script SQL documentado para consultar las tablas de catálogo (pg_constraint, pg_attribute, pg_index) con el fin de listar estas claves foráneas no indexadas.
    - Se añadió la función check_missing_indexes(conn) para consultar e identificar mediante Python esta problemática.

--- backend/detectors/indexes.py
+++ backend/detectors/indexes.py
@@ -9,3 +9,43 @@ Incluye:
 
 """
 
+import psycopg2
+from psycopg2 import extras
+
+
+def check_missing_indexes(conn):
+    """
+    Detecta llaves foráneas que no tengan un índice asociado.
+    """
+    query = """
+    SELECT 
+        conname AS constraint_name,
+        conrelid::regclass AS table_name,
+        a.attname AS column_name,
+        confrelid::regclass AS referenced_table
+    FROM 
+        pg_constraint c
+    JOIN 
+        pg_attribute a ON a.attnum = ANY(c.conkey) AND a.attrelid = c.conrelid
+    LEFT JOIN 
+        pg_index i ON i.indrelid = c.conrelid 
+                   AND a.attnum = i.indkey[0]
+    WHERE 
+        c.contype = 'f' 
+        AND i.indexrelid IS NULL;
+    """
+    
+    missing_indexes = []
+    
+    try:
+        # Se usa RealDictCursor para que el resultado para que devuelva 
+        # el resultado como diccionarios
+        with conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cur:
+            cur.execute(query)
+            missing_indexes = cur.fetchall()
+            
+    except Exception as e:
+        print(f"Error al consultar los índices faltantes: {e}")        
+        raise e
+        
+    return missing_indexes
--- backend/discovery_queries/indexes.sql
+++ backend/discovery_queries/indexes.sql
@@ -7,3 +7,36 @@ ineficiencias en la estructura de indexación, tales como
 la ausencia de índices en llaves foráneas que provocan escaneos secuenciales.
 */
 
+
+/*
+Problema: Detección de llaves foráneas sin índice
+
+Este query tiene como objetivo principal identificar las columnas 
+que actúan como llaves foráneas que no tienen un indice asociado en 
+la tabla donde se encuentra. 
+
+Referencias: 
+PostgreSQL. (2026). 52.13. pg_constraint. PostgreSQL 18 Documentation. https://www.postgresql.org/docs/18/catalog-pg-constraint.html
+PostgreSQL. (2026). 52.7. pg_attribute. PostgreSQL 18 Documentation. https://www.postgresql.org/docs/18/catalog-pg-attribute.html
+PostgreSQL. (2026). 52.26. pg_index. PostgreSQL 18 Documentation. https://www.postgresql.org/docs/18/catalog-pg-index.html
+*/
+
+SELECT 
+    conname AS constraint_name, -- Nombre de la restricción
+    conrelid::regclass AS table_name, -- La tabla donde está la FK
+    a.attname AS column_name, -- El nombre de la columna que es la FK
+    confrelid::regclass AS referenced_table, -- La tabla a la que apunta esta FK
+    CASE 
+        WHEN i.indexrelid IS NOT NULL THEN 'Si'	-- Si es que tiene un índice
+        ELSE 'No'
+    END AS tiene_indice
+FROM 
+    pg_constraint c
+JOIN 
+    pg_attribute a ON a.attnum = ANY(c.conkey) AND a.attrelid = c.conrelid
+LEFT JOIN 
+    pg_index i ON i.indrelid = c.conrelid AND a.attnum = i.indkey[0] -- Checa si es la primera columna del índice
+WHERE 
+    c.contype = 'f' -- Se usa f para filtrar las llaves foráneas
+ORDER BY 
+    tiene_indice ASC, table_name;
