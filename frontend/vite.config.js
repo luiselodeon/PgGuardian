f716afb80a0de66e75874a8f2170506c6e50a250
LuisArias <luisariasllaguno@gmail.com>
2026-05-11 23:22:15 -0600

    Modifiación index y vite config
    - Se modificó el archivo de index.html para que tenga la información correcta de PgGuardian, aparte de agregar los fonts de google al head
    - se modificó vite.config.js para que pueda usar en docker y que se pueda ver los cambios en tiempo real con hot reload en el front

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  //Se pone para que funcione en Docker
+    port: 5173, 
+    watch:{
+      usePolling: true //Se usa esto para que se pueda recargar los cambios en hot reload
+    },
+  },
 })
